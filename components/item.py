15050cdbf8779f6933678a1954135917064fafc3
Firas <firasaldabbas+git@gmail.com>
2019-07-01 22:13:59 -0400

    Updated item to allow for targeting

--- components/item.py
+++ components/item.py
@@ -1,6 +1,7 @@
 class Item:
-    def __init__(self, use_function=None, owner=None, **kwargs):
+    def __init__(self, use_function=None, owner=None, targeting=False, targeting_message=None, **kwargs):
         self.owner = owner
         self.use_function = use_function
         self.function_kwargs = kwargs
-        
+        self.targeting = targeting
+        self.targeting_message = targeting_message
